8d41379dce69281f748d159d79b98b90734b0d2f
Marlena Luczak <dziubichmarlena@gmail.com>
2023-07-05 15:57:42 +0200

    next methods with conditional statements

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -81,20 +81,20 @@ public class Exercise {
     // 8. Use a conditional statement to return "Correct!" if numOne is more than or equal to numTwo
     // or "Wrong!" if not
     public String eight(int numOne, int numTwo) {
-        return "Not implemented yet!";
+        return numOne >= numTwo ? "Correct!" : "Wrong!";
     }
 
     // 9. Use a conditional statement to return true if the array provided is not empty
     // or false if it is empty
     public boolean nine(int[] nums) {
-        return false;
+        return nums.length != 0;
     }
 
     // 10. Use a conditional statement to return true if the provided string contains the word
     // "milk", or false if not
     // https://www.w3schools.com/java/java_ref_string.asp
     public boolean ten(String sentence) {
-        return false;
+        return sentence.contains("milk");
     }
 
     // 11. Use conditional statements to return the number 3 if the provided string contains
@@ -102,13 +102,21 @@ public class Exercise {
     // Return the number 9 if the string contains both coffee and milk.
     // Otherwise, return the number 0.
     public int eleven(String sentence) {
-        return -1;
+        if(sentence.contains("milk") && sentence.contains("coffee")){
+            return 9;
+        } else if(sentence.contains("milk")){
+            return 3;
+        } else if(sentence.contains("coffe")){
+            return 6;
+        } else {
+            return 0;
+        }
     }
 
     // 12. Use conditional statements to return true if num is more than or equal to lower and is
     // less than or equal to upper, otherwise return false.
     public boolean twelve(int num, int lower, int upper) {
-        return false;
+        return num >= lower && num <= upper;
     }
 
     /*
@@ -124,6 +132,16 @@ public class Exercise {
         20+     | Adult
      */
     public String thirteen(int age) {
-        return "Not implemented yet!";
+        if(age == 0){
+            return "Baby";
+        } else if(age>= 1 && age <5){
+            return "Toddler";
+        }else if(age>= 5 && age <13){
+            return "Child";
+        }else if(age>= 13 && age <20){
+            return "Teenager";
+        } else {
+            return "Adult";
+        }
     }
 }
