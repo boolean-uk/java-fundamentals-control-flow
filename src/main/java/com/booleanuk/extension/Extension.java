5fa2b626d963f6c4c77772cefb2521ca24c77fcc
Wojciech Żółkowski <wojciechzolkowski@gmail.com>
2023-07-05 13:07:04 +0200

    First commit

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -14,7 +14,16 @@ public class Extension extends ExtensionBase {
         and "The timer finished ages ago!" if the remaining minutes is a negative number
      */
 
-
+        public String timerStatus(int minutes){
+            if(minutes == 0){
+                return "The cake is ready";
+            }
+            if (minutes > 0){
+                return  "The cake is still baking!";
+            }else{
+                return "The timer finished ages ago!";
+            }
+        }
 
 
     /*  2.
@@ -26,6 +35,14 @@ public class Extension extends ExtensionBase {
         provided and the prep time per ingredient.
         If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
      */
+
+        public int estimatePrepTime(String[] ingredients, int minutes){
+            if(minutes == 0){
+                return ingredients.length * 2;
+            }else{
+                return ingredients.length * minutes;
+            }
+        }
 
 
 
