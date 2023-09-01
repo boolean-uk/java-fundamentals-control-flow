14b387f66189755308a506588451e6fcd0f21b2f
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-01 13:31:15 +0200

    Finished the assignments

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -2,6 +2,8 @@ package com.booleanuk.extension;
 
 import com.booleanuk.helpers.ExtensionBase;
 
+import java.util.ArrayList;
+
 public class Extension extends ExtensionBase {
     /*  1.
         We're going to improve our cake baking capabilities!
@@ -13,8 +15,16 @@ public class Extension extends ExtensionBase {
         "The cake is still baking!" if there are any remaining minutes left,
         and "The timer finished ages ago!" if the remaining minutes is a negative number
      */
+    public String timerStatus(int min) {
+        if (min == 0) {
+            return "The cake is ready!";
 
-
+        } else if (min > 0) {
+            return "The cake is still baking!";
+        } else {
+            return "The timer finished ages ago!";
+        }
+    }
 
 
     /*  2.
@@ -26,7 +36,18 @@ public class Extension extends ExtensionBase {
         provided and the prep time per ingredient.
         If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
      */
+    public int estimatePrepTime(String[] ing, int min) {
+        int ingredients = ing.length;
+        int prepInMin = min;
 
+        if (prepInMin == 0) {
+            prepInMin = 2;
+            return prepInMin * ingredients;
+        } else {
+            return prepInMin * ingredients;
+        }
+
+    }
 
 
     /*  3.
@@ -41,7 +62,19 @@ public class Extension extends ExtensionBase {
         You may need to use programming techniques we have yet to cover in the course to solve this task.
      */
 
+    public int calculateGramsOfSugar(String[] ing, int layers) {
+        int sugarAmount = 0;
 
+        for (int i = 0; i < ing.length; i++) {
+            if (ing[i].contains("sugar")) {
+                sugarAmount = 100;
+                break;
+            }
+        }
 
+        return sugarAmount * layers;
 
+    }
+
+//Version control 13.30
 }
