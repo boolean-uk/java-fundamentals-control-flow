41596a2f2c93f5ed9522deaf3a800d7ee78ad0ef
Espen Solhaug <espen.solhaug@no.experis.com>
2024-01-10 15:18:37 +0100

    Finished Exercise & Extension

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -14,6 +14,15 @@ public class Extension extends ExtensionBase {
         and "The timer finished ages ago!" if the remaining minutes is a negative number
      */
 
+    public String timerStatus(int minutesLeft) {
+        if(minutesLeft == 0) {
+            return "The cake is ready!";
+        } else if(minutesLeft > 0) {
+            return "The cake is still baking!";
+        } else
+            return "The timer finished ages ago!";
+    }
+
 
 
 
@@ -26,7 +35,12 @@ public class Extension extends ExtensionBase {
         provided and the prep time per ingredient.
         If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
      */
-
+    public int estimatePrepTime(String[] ingredients, int minutesPerIngredient) {
+        if(minutesPerIngredient == 0) {
+            minutesPerIngredient = 2;
+        }
+        return ingredients.length * minutesPerIngredient;
+    }
 
 
     /*  3.
@@ -40,7 +54,18 @@ public class Extension extends ExtensionBase {
 
         You may need to use programming techniques we have yet to cover in the course to solve this task.
      */
-
+    public int calculateGramsOfSugar(String[] ingredients, int layers) {
+        boolean containsSugar = false;
+        for(String ingredient : ingredients) {
+            if(ingredient.equals("sugar")) {
+                containsSugar = true;
+            }
+        }
+        if(containsSugar) {
+            return 100 * layers;
+        }
+        return 0;
+    }
 
 
 
