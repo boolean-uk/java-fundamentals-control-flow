0eeebbd6528f36bb8331d738da37be193f3acd5b
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-08-05 13:35:15 +0200

    implement exercises

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -2,6 +2,8 @@ package com.booleanuk.extension;
 
 import com.booleanuk.helpers.ExtensionBase;
 
+import java.util.Arrays;
+
 public class Extension extends ExtensionBase {
     /*  1.
         We're going to improve our cake baking capabilities!
@@ -13,6 +15,15 @@ public class Extension extends ExtensionBase {
         "The cake is still baking!" if there are any remaining minutes left,
         and "The timer finished ages ago!" if the remaining minutes is a negative number
      */
+    public String timerStatus(int time) {
+        if (time == 0) {
+            return "The cake is ready!";
+        } else if (time >= 0) {
+            return "The cake is still baking!";
+        } else {
+            return "The timer finished ages ago!";
+        }
+    }
 
 
 
@@ -27,6 +38,13 @@ public class Extension extends ExtensionBase {
         If a prep time of 0 is provided, the method should assume each ingredient takes 2 minutes to prepare.
      */
 
+    public int estimatePrepTime(String[] ingredients, int prepTimePer) {
+        if (prepTimePer == 0) {
+            prepTimePer = 2;
+        }
+        return ingredients.length *  prepTimePer;
+    }
+
 
 
     /*  3.
@@ -40,7 +58,13 @@ public class Extension extends ExtensionBase {
 
         You may need to use programming techniques we have yet to cover in the course to solve this task.
      */
-
+    public int calculateGramsOfSugar(String[] ingredients, int layers) {
+        if (Arrays.stream(ingredients).anyMatch(x -> x.contains("sugar"))) {
+            return layers * 100;
+        } else {
+            return 0;
+        }
+    }
 
 
 
